9907b4ca8cb48ac14ccee90819231bc4a47595aa
DickyAulia77 <113884029+DickyAulia77@users.noreply.github.com>
2022-11-14 13:50:06 +0800

    Delete Donor-Dicky directory

--- Donor-Dicky/JS/loginuser.js
+++
@@ -1,7 +0,0 @@
-const signinBtn = document.querySelector('.signinBtn')
-const signupBtn = document.querySelector('.signupBtn')
-const formWX = document.querySelector('.formWx')
-
-signupBtn.onclick = function() {
-    formWX.classList.add('active')
-}
